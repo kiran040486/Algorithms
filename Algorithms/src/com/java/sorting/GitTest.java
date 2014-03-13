f95214e6aac50cc366b7d585ad1722410ce2829b
kiran040486 <kiran040486@gmail.com>
2014-03-13 12:43:48 -0700

    Testing Git Integration with Eclipse

---
+++ Algorithms/src/com/java/sorting/GitTest.java
@@ -0,0 +1,12 @@
+package com.java.sorting;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is to test Git integration with eclipse :");
+
+	}
+
+}
